2066f6e9a97a092f411c5591d66375c82098abbd
dorjepadma <dorjepadma@gmail.com>
2020-02-19 17:43:13 -0800

    weather and location working

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const request = require('superagent');
-// const data = require('./data/geo.js');
-// const weather = require('./darksky.js');
 const cors = require('cors');
 const app = express();
 
@@ -15,17 +13,17 @@ let lng;
 app.get('/location', async(req, res, next) => {
     try { const location = req.query.search;
 
-    const URL = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${location}&format=json`;    
+    const URL = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEO_CODE_API}&q=${location}&format=json`;    
     
     const cityData = await request.get(URL);
 
     const firstResult = cityData.body[0];
 
     lat = firstResult.lat;
-    lng = firstResult.lng;
+    lng = firstResult.lon;
 
     res.json({
-        formatted_query: cityData.formatted_address,
+        formatted_query: firstResult.display_name,
         latitude: lat,
         longitude: lng,
     });
@@ -34,21 +32,23 @@ app.get('/location', async(req, res, next) => {
 }
 });
 const getWeatherData = async (lat, lng) => {
-    const weather = await request.get(`
-    https://api.darksky.net/forecast/${WEATHER_KEY}/${lat},${long}`);
-    
+    const weather = await request.get(`https://api.darksky.net/forecast/${process.env.WEATHER_KEY}/${lat},${lng}`);
     
     return weather.body.daily.data.map(forecast => {
         return {
         forecast: forecast.summary,
         time: new Date(forecast.time * 1000),
-    };
+        };
     });
-};
-        
-        app.get('/weather', (req, res, next) => {
-    const portlandWeather = getWeatherData(lat, lng);
+} ;
+// console.log(forecast);
+        app.get('/weather', async(req, res, next) => {
+try {
+    const portlandWeather = await getWeatherData(lat, lng);
             res.json(portlandWeather);
+} catch (err) {
+    next(err)
+}
 });
 app.get('*', (req, res) => res.send('404!!!'))
 module.exports = {
